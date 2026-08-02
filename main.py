a10b91912c5ca7b1a2f5ef77b8c705251ac63678
flaviamedici <flaviaomedici@gmail.com>
2026-08-02 08:55:30 -0700

    Getting top headlines for any country

--- main.py
+++ main.py
@@ -19,3 +19,16 @@ def get_news(topic, from_date, to_date, language='en', api_key = 'ac1014efb79c4a
         return results
 
     print(get_news(topic='space', from_date='2022-2-27', to_date='2022-2-28'))
+
+
+def get_news_headlines(country, api_key = 'ac1014efb79c4aa5a63c7f40de83ad4a'):
+    url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={api_key}'
+    r = requests.get(url)
+    content = r.json()
+    articles = content['articles']
+    results = []
+    for article in articles:
+        results.append(f"TITLE\n' {article['title']}, '\nDESCRIPTION\n' {article['description']}")
+        return results
+
+    print(get_news(country='us'))
